277b84de8f79454396ac22448b709c84c86b9885
Yerassyl-Assyltay <104434172+Yerassyl-Assyltay@users.noreply.github.com>
2022-05-01 01:14:11 +0600

    Update Activity_11_21.6.java

--- Activity_11_21.6.java
+++ Activity_11_21.6.java
@@ -1,1 +1,39 @@
+import java.util.Scanner;
+public class ex21_06 {
 
+    public static void main(String[] args) {
+
+        //declarations
+        int[] myArray = new int[100];
+        int input = 5;
+        Scanner keyboard = new Scanner(System.in);
+
+        //input and processing
+        System.out.println("Please enter integers between 1 and 100 (enter 0 to stop): ");
+        while (input != 0) {
+
+            input = keyboard.nextInt();
+
+            for (int i = 0; i < myArray.length; i++) {
+                if (input == i) {
+                    myArray[i] = input;
+                }
+            }
+
+        }
+
+        //output (This is where I need help!!!!)
+        for (int k = 0; k < myArray.length; k++) {
+            if (myArray[k] != 0) {
+
+                System.out.print(k + " occurs " + myArray[k] + " time");
+                if (myArray[k] > 1) {
+                    System.out.println("s");
+                } else
+                    System.out.println();
+
+            }
+        }
+        keyboard.close();
+    }
+}
